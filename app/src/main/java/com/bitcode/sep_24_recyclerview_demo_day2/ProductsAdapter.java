9502be99db427d2a13673ccd47bd17bc6855495f
aishwarya <ashp17star@gmail.com>
2024-10-10 17:02:20 +0530

    .

--- app/src/main/java/com/bitcode/sep_24_recyclerview_demo_day2/ProductsAdapter.java
+++ app/src/main/java/com/bitcode/sep_24_recyclerview_demo_day2/ProductsAdapter.java
@@ -1,5 +1,7 @@
 package com.bitcode.sep_24_recyclerview_demo_day2;
 
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,7 +27,17 @@ public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.Produc
         Product product;
         public ProductViewHolder(@NonNull View itemView) {
             super(itemView);
+            imgView = itemView.findViewById(R.id.imgView);
 
+            imgView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //getAdapterPosition is the method to get position specific data
+                        product = productArrayList.get(getAdapterPosition());
+                        Log.e("tag" , product.productImage + "--" + product.productId + "--"
+                                + product.productTitle + "--" + product.productPrice);
+                }
+            });
         }
     }
 
@@ -39,6 +51,26 @@ public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.Produc
        txtPrice = productView.findViewById(R.id.txtPrice);
        imgView = productView.findViewById(R.id.imgView);
 
+//       txtTitle.setOnClickListener(new View.OnClickListener() {
+//           @Override
+//           public void onClick(View view) {
+//               Log.e("tag", "txt Title");
+//           }
+//       });
+//
+//       txtPrice.setOnClickListener(new View.OnClickListener() {
+//           @Override
+//           public void onClick(View view) {
+//                Log.e("tag", "txt Price");
+//           }
+//       });
+//
+//       imgView.setOnClickListener(new View.OnClickListener() {
+//           @Override
+//           public void onClick(View view) {
+//                Log.e("tag", "img View");
+//           }
+//       });
         return new ProductViewHolder(productView);
     }
 
@@ -53,6 +85,32 @@ public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.Produc
           txtTitle.setText("" + productToBeBound.productTitle);
           txtPrice.setText("" + productToBeBound.productPrice);
           imgView.setImageResource(R.drawable.ic_launcher_background);
+
+//          txtTitle.setOnClickListener(new View.OnClickListener() {
+//              @Override
+//              public void onClick(View view) {
+//                    Log.e("tag", "Product Title " + productToBeBound.productTitle);
+//
+//                    //passing Data using Intent
+//                  Intent intent = new Intent(view.getContext(),ProductDetailsActivity.class);
+//                  intent.putExtra("title", productToBeBound.productTitle);
+//                  view.getContext().startActivity(intent);
+//              }
+//          });
+//
+//          txtPrice.setOnClickListener(new View.OnClickListener() {
+//              @Override
+//              public void onClick(View view) {
+//                    Log.e("tag", "Product Price " + productToBeBound.productPrice);
+//              }
+//          });
+//
+//          imgView.setOnClickListener(new View.OnClickListener() {
+//              @Override
+//              public void onClick(View view) {
+//                  Log.e("tag", "Product Image " + productToBeBound.productImage);
+//              }
+//          });
     }
 
     @Override
